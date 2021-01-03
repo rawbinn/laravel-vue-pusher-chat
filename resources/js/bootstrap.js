326830e6cda798e88281e202755ae3c520d70439
Robinhood Shrestha <rawbinnn@gmail.com>
2021-01-03 16:02:25 +0545

    authorization added to header

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('auth_token');
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
